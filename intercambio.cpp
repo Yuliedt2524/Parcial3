fa54682bd672812592b76115cdaa3962cbd96ca4
Yuliedt2524 <anayuliedts@gmail.com>
2025-05-20 08:05:48 -0600

    Add files via upload

---
+++ intercambio.cpp
@@ -0,0 +1,21 @@
+    #include <stdio.h>
+    # include <stdlib.h>
+    
+        void intercambio(int *a,int *b){
+    int temp;
+    temp=*b;
+    *b=*a;
+    *a=temp;
+ } 
+   
+
+    int main(void) {
+    int x=2;
+    int y=5; 
+    printf("antes x=%d, y=%d\n",x,y);
+    intercambio(&x, &y);
+    printf("despues x=%d, y=%d\n",x,y);
+    return 0;
+    }
+    
+    
